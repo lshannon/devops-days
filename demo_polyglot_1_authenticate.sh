6dff79c1a61c47b5434c474c3e4c082e49d1673a
lshannon <luke.shannon@gmail.com>
2016-04-16 15:40:39 -0700

    Added args for authentication

--- demo_polyglot_1_authenticate.sh
+++ demo_polyglot_1_authenticate.sh
@@ -12,13 +12,11 @@ echo " "
 echo "Running: https://api.run.pez.pivotal.io"
 echo "This is running in an onpremise vSphere Cluster"
 echo " "
-#if [ $# -eq 0 ]; then
-#	echo "Usage: demo_polymorphic_1.sh <organization> <space>";
-#	echo "Hint: demo_polymorphic_1.sh pivot-lshannon devops-days";
-#	echo "Program terminating ...";
-#	exit 1;
-#fi
+if [ $# -eq 0 ]; then
+	echo "Usage: demo_polymorphic_1.sh <organization> <space>";
+	echo "Program terminating ...";
+	exit 1;
+fi
 cf api https://api.run.pez.pivotal.io
 echo "Running: cf login -sso"
-#cf login -o "$1" -s "$2" -sso
-cf login -o pivot-lshannon -s devops-days -sso
+cf login -o "$1" -s "$2" -sso
